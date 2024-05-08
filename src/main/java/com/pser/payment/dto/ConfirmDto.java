618771e7dfc18b0ea2f7340cb4b1646f4ba577bf
amanecopse <amanecopse@gmail.com>
2024-05-08 20:21:32 +0900

    refactor: 카프카 DTO 생성자 추가

--- src/main/java/com/pser/payment/dto/ConfirmDto.java
+++ src/main/java/com/pser/payment/dto/ConfirmDto.java
@@ -2,13 +2,17 @@ package com.pser.payment.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfirmDto {
     @JsonAlias("apply_num")
     private String applyNum;
--- src/main/java/com/pser/payment/dto/PaymentDto.java
+++ src/main/java/com/pser/payment/dto/PaymentDto.java
@@ -1,13 +1,17 @@
 package com.pser.payment.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentDto {
     private int code;
     private String message;

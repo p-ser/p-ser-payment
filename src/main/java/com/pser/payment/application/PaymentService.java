64d36c352e4c38096368b6fddf50b5bc553112a2
amanecopse <amanecopse@gmail.com>
2024-05-13 11:38:06 +0900

    feat: 결제 상태 검증 로직 및 롤백 추가

--- src/main/java/com/pser/payment/application/PaymentService.java
+++ src/main/java/com/pser/payment/application/PaymentService.java
@@ -3,7 +3,9 @@ package com.pser.payment.application;
 import com.pser.payment.domain.ServiceEnum;
 import com.pser.payment.dto.ConfirmDto;
 import com.pser.payment.dto.RefundDto;
+import com.pser.payment.exception.ValidationFailedException;
 import com.pser.payment.infra.PortoneClient;
+import com.pser.payment.producer.ConfirmAwaitingRollbackProducer;
 import com.pser.payment.producer.ConfirmedProducer;
 import com.pser.payment.producer.RefundedProducer;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +18,25 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
     private final ConfirmedProducer confirmedProducer;
     private final RefundedProducer refundedProducer;
+    private final ConfirmAwaitingRollbackProducer confirmAwaitingRollbackProducer;
     private final PortoneClient portoneClient;
 
     public void confirm(ServiceEnum serviceEnum, ConfirmDto confirmDto) {
         String impUid = confirmDto.getImpUid();
 
         portoneClient.tryGetByImpUid(impUid)
+                .map(paymentDto -> {
+                    String status = paymentDto.getResponse().getStatus();
+                    if (status == null || !status.equals("paid")) {
+                        throw new ValidationFailedException();
+                    }
+                    return paymentDto;
+                })
                 .onSuccess((paymentDto) -> confirmedProducer.notifyConfirmed(serviceEnum, paymentDto))
+                .recover(ValidationFailedException.class, e -> {
+                    confirmAwaitingRollbackProducer.notifyRollback(serviceEnum, confirmDto.getMerchantUid());
+                    return null;
+                })
                 .get();
     }
 
---
+++ src/main/java/com/pser/payment/producer/ConfirmAwaitingRollbackProducer.java
@@ -0,0 +1,17 @@
+package com.pser.payment.producer;
+
+import com.pser.payment.domain.ServiceEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ConfirmAwaitingRollbackProducer {
+    private final KafkaTemplate<String, String> stringValueKafkaTemplate;
+
+    public void notifyRollback(ServiceEnum serviceEnum, String merchantUid) {
+        String topic = serviceEnum.getTopicPrefix() + ".confirm-awaiting-rollback";
+        stringValueKafkaTemplate.send(topic, merchantUid);
+    }
+}

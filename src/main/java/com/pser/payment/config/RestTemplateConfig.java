d549f162c3020bfeaed3b6615fa27b6c4525cd5c
amanecopse <amanecopse@gmail.com>
2024-06-05 11:55:15 +0900

    fix: RestClient 요청 버퍼 문제 해결
    요청 버퍼 문제로 요청 데이터가 무작위로 잘못들어가는 현상을 해결함

--- src/main/java/com/pser/payment/config/RestTemplateConfig.java
+++ src/main/java/com/pser/payment/config/RestTemplateConfig.java
@@ -1,14 +1,17 @@
 package com.pser.payment.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate() {
+        ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+        return new RestTemplate(factory);
     }
 }
